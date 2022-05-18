42c55ef8ca3e4fc93bd5dee33f4acc27b91d5a0e
Raspberry <christiantejada31@gmail.com>
2022-05-18 02:01:54 -0400

    fix lint issues

--- src/Pages/Comments.js
+++ src/Pages/Comments.js
@@ -1,4 +1,4 @@
-import { Divider, Paper, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import React from "react";
 import { useLocation } from "react-router-dom";
